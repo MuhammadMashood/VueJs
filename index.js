f1b96f01891e0a0d7bb8a7e2c12deea6bea19916
Muhammad <mashoodshaukat@gmail.com>
2021-02-24 23:46:08 +0500

    last commit of day (24 Feb)

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ var app = new Vue({
     data: {
         temp: 0,
         message: 'Hi I am a message',
+        text: '',
+        gate: false,
+        students: ['Muhammad', 'Arshad', 'qasim', 'Yaseen'],
+        players:[
+            {name:'Qasim' ,age:22},
+            {name:"Fahad" ,age:18},
+            {name:'Murtazaa' ,age: 50},
+        ]
 
     },
     methods: {
